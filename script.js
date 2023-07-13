8efa58f29a7fd7f8e9eec78e8597c0545d521e76
Hannah Comparetto <hannahcomparetto@gmail.com>
2023-07-13 16:52:55 -0500

    create ready function for DOM

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('🟥 🟦 🟩 🟨');
+
+//First establish that once browser is loaded to begin anything listed in onReady function
+$(document).ready(onReady);
+
+//second create ready function
+function onReady() {
+
+    
+}
